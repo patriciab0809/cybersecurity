ee8146241f3678020ac5a6b5b3bf9d2c3532e5a9
Patricia Booker <patriciabooker2015@gmail.com>
2024-08-21 18:40:49 -0500

    103.3.sh

---
+++ 103.3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Condtinal Statment 
+# This ops challenges is about conditinal stamtment of if else statments and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+numbers= 7
