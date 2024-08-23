77acda7e0c0fe6e9cf757981c9199e265367a5b5
Patricia Booker <patriciabooker2015@gmail.com>
2024-08-22 19:10:43 -0500

    103.4.sh

---
+++ 103.4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Lets create a function in bash that adds two number together
+# Stretch goal can you do subtraction, multipliaction or division
+# You will need to declare two variables
+
+
+function hello(){
+    ipconfig
+    echo "How are you doing"
+    Ls  
+ }
+
+
+ function add (){
+    num1=6
+    num2=7
+    echo $((num1 + num2))
+    }
+ add 
